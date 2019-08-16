0681c10e1db4056283cb7055dbf61874164c2355
satonaka214 <spt85wx9@gmail.com>
2019-08-16 12:57:23 +0900

    first commit

---
+++ src/test/java/com/satonaka214/githubtest/GithubtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.satonaka214.githubtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
